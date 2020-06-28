818b8070b1958f74f9a06d88f8b713456f8e1acb
a86913179 <a86913179@gmail.com>
2020-06-28 19:58:16 +0800

    Update clash

--- tun2socks.bat
+++ tun2socks.bat
@@ -1,3 +1,3 @@
 @echo off
 
-tun2socks.exe -proxyServer 127.0.0.1:7891 -tunDns 10.255.0.2 -tunName clash-tap > tun2socks.log
+tun2socks.exe -proxyServer 127.0.0.1:7890 -tunDns 10.255.0.2 -tunName clash-tap > tun2socks.log
